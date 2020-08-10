9617cdb0f5d2abf2c28112725e2b35c5a592f785
chongqu <1606548133@qq.com>
2020-08-10 10:06:26 +0800

    登录

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -12,7 +12,7 @@ class LoginController extends Controller
     {
         //登录成功,记录cookie
         $shop_info = ShopModel::where('shop_id', $request->shop_id)->first();
-        if (md5($request->shop_pwd) == $shop_info->shop_pwd) {
+        if ($shop_info&&md5($request->shop_pwd) == $shop_info->shop_pwd) {
             session(['login:' . config('inm.h5admin_auth_key') => $shop_info->shop_id]);
             return $this->output(null,'登录成功','200');
         } else {
